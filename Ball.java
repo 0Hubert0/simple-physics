33de6e20432ab374fe3bcf97d67050b9a8986e42
0Hubert0 <95587852+0Hubert0@users.noreply.github.com>
2023-10-13 23:21:59 +0100

    Update Ball.java

--- Ball.java
+++ Ball.java
@@ -4,12 +4,34 @@ import javafx.scene.shape.Circle;
 
 public class Ball extends Circle{
     double vx, vy;
+    int weight;
 
     Ball (double x, double y)
     {
         this.setCenterX(x);
         this.setCenterY(y);
         this.setRadius((int)(Math.random()*3)*20);
+        this.weight = (int)(this.getRadius()/20);
+    }
+
+    public double getVx(){
+        return this.vx;
+    }
+
+    public void setVx(double vx){
+        this.vx = vx;
+    }
+
+    public double getVy(){
+        return this.vy;
+    }
+
+    public void setVy(double vy){
+        this.vy = vy;
+    }
+
+    public int getWeight(){
+        return this.weight;
     }
 
     public boolean checkColision(Ball b){
@@ -19,5 +41,11 @@ public class Ball extends Circle{
             (this.getRadius()+b.getRadius())*(this.getRadius()+b.getRadius()))
             return true;
         return false;
+    }
+
+    public void disableCollision(Ball b){
+        double distance = Math.sqrt(b.getCenterX()-this.getCenterX())*(b.getCenterX()-this.getCenterX())+
+            (b.getCenterY()-this.getCenterY())*(b.getCenterY()-this.getCenterY());
+        double angle = b.getCenterX();
     }
 }
